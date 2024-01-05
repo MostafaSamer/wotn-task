3fc48498738e8703f64e641aabe88f5b816e7bf5
root <root@mostafa-pc>
2024-01-06 01:47:38 +0200

    finish modal

--- script.js
+++ script.js
@@ -14,4 +14,28 @@ document.addEventListener('DOMContentLoaded', function () {
     //     card.textContent = 'Card ' + i;
     //     cardsContainer.appendChild(card);
     // }
+
+    const openModalBtns = document.querySelectorAll('.openModalBtn');
+    const closeModalBtn = document.getElementById('closeModalBtn');
+    const modal = document.getElementById('myModal');
+
+    function openModal() {
+        modal.style.display = 'flex';
+    }
+
+    function closeModal() {
+        modal.style.display = 'none';
+    }
+
+    openModalBtns.forEach(function (btn) {
+        btn.addEventListener('click', openModal);
+    });
+
+    closeModalBtn.addEventListener('click', closeModal);
+
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
 });
