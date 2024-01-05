b3812fee4881074c69f14138ee368d1fa77eb8d7
root <root@mostafa-pc>
2024-01-05 21:39:57 +0200

    card-header

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('menu-toggle');
     const headerLinks = document.getElementById('header-links');
-
     menuToggle.addEventListener('click', function () {
         headerLinks.classList.toggle('show');
     });
+    const cardsContainer = document.getElementById('cards-container');
+
+
+    // const numberOfCards = 5;
+    // for (let i = 1; i <= numberOfCards; i++) {
+    //     const card = document.createElement('div');
+    //     card.className = 'card';
+    //     card.textContent = 'Card ' + i;
+    //     cardsContainer.appendChild(card);
+    // }
 });
